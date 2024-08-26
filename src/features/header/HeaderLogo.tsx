640e0f8cd507e67f7513aafc138e6f2584d073e1
Sandro <vipsandro11@gmail.com>
2024-08-26 19:55:32 +0400

    update HeaderLogo.tsx

--- src/features/header/HeaderLogo.tsx
+++ src/features/header/HeaderLogo.tsx
@@ -6,7 +6,9 @@ const HeaderLogo = () => {
       <div className="flex items-center">
         <img src={logo} alt="logo" className="w-[8rem] h-auto py-5" />
         <div className="w-[1px] h-full bg-primary mx-5 py-5">&nbsp;</div>
-        <p className="text-primary text-[.6rem] font-semibold">გააუმჯობესეთ თქვენი კარიერული გზა</p>
+        <p className="text-primary text-[.6rem] font-semibold italic">
+          გააუმჯობესეთ თქვენი კარიერული გზა
+        </p>
       </div>
     </div>
   );
