85356c9a783964aa111b55f9f7c4eb76ffd7bb40
Sandro <vipsandro11@gmail.com>
2024-08-27 16:56:41 +0400

    update MainLayout.tsx

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../features/navbar/Navbar";
 import HeaderLogo from "../features/header/HeaderLogo";
+import Filters from "../features/filters/Filters";
 
 const MainLayout = () => {
   return (
     <main>
       <HeaderLogo />
       <Navbar />
+      <Filters />
       <Outlet />
     </main>
   );
