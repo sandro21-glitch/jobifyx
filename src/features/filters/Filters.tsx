3156f22a3f27969166b3b6b489bfa550e0d85332
Sandro <vipsandro11@gmail.com>
2024-08-27 16:56:31 +0400

    update Filters.tsx

--- src/features/filters/Filters.tsx
+++ src/features/filters/Filters.tsx
@@ -1,8 +1,15 @@
+import VacancyCategory from "./filterTypes/VacancyCategory";
+import VacancyLocation from "./filterTypes/VacancyLocation";
+import VacancyType from "./filterTypes/VacancyType";
 
 const Filters = () => {
   return (
-    <div>Filters</div>
-  )
-}
+    <div className="section-center flex justify-end gap-5">
+      <VacancyType />
+      <VacancyLocation />
+      <VacancyCategory />
+    </div>
+  );
+};
 
-export default Filters
+export default Filters;
