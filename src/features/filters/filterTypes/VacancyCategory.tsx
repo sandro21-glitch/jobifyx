4a671a078d40ad22bdace20ebe4f310f624315ad
Sandro <vipsandro11@gmail.com>
2024-08-27 16:56:34 +0400

    update VacancyCategory.tsx

--- src/features/filters/filterTypes/VacancyCategory.tsx
+++ src/features/filters/filterTypes/VacancyCategory.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const VacancyCategory = () => {
-  return <div>VacancyCategory</div>;
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const vacancyCategories = [
+    "ყველა ვაკანსია",
+    "ვაკანსიები",
+    "სტიპენდიები",
+    "სხვა",
+    "ტენდერები",
+    "ტრენინგები",
+  ];
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  return (
+    <div>
+      <select
+        id="selectedCategory"
+        value={selectedCategory}
+        onChange={handleChange}
+        className="text-[.6rem] border p-[1px] bg-background outline-none"
+      >
+        {vacancyCategories.map((type, index) => (
+          <option key={index} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default VacancyCategory;
