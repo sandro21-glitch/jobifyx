e99e67b64874721a81a4f6d73e44a9fd8141aa30
Sandro <vipsandro11@gmail.com>
2024-08-27 16:56:37 +0400

    update VacancyLocation.tsx

--- src/features/filters/filterTypes/VacancyLocation.tsx
+++ src/features/filters/filterTypes/VacancyLocation.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const VacancyLocation = () => {
-  return <div>VacancyLocation</div>;
+  const [selectedLocation, setSelectedLocation] = useState("");
+
+  const vacancyLocations = [
+    "ყველა ვაკანსია",
+    "ვაკანსიები",
+    "სტიპენდიები",
+    "სხვა",
+    "ტენდერები",
+    "ტრენინგები",
+  ];
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedLocation(event.target.value);
+  };
+
+  return (
+    <div>
+      <select
+        id="selectedLocation"
+        value={selectedLocation}
+        onChange={handleChange}
+        className="text-[.6rem] border p-[1px] bg-background outline-none"
+      >
+        {vacancyLocations.map((type, index) => (
+          <option key={index} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default VacancyLocation;
