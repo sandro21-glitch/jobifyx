59c4a33b6fd09be95cdb8a34df627ad2d2ca9f49
Sandro <vipsandro11@gmail.com>
2024-09-01 17:24:30 +0400

    update Filters.tsx

--- src/features/filters/Filters.tsx
+++ src/features/filters/Filters.tsx
@@ -1,13 +1,20 @@
+import InputFilter from "./filterTypes/InputFilter";
 import VacancyCategory from "./filterTypes/VacancyCategory";
 import VacancyLocation from "./filterTypes/VacancyLocation";
 import VacancyType from "./filterTypes/VacancyType";
 
 const Filters = () => {
   return (
-    <div className="section-center flex justify-end gap-5">
-      <VacancyType />
-      <VacancyLocation />
-      <VacancyCategory />
+    <div
+      style={{ marginTop: "15px" }}
+      className="section-center flex justify-end"
+    >
+      <div className="flex items-center gap-5">
+        <VacancyType />
+        <VacancyLocation />
+        <VacancyCategory />
+        <InputFilter />
+      </div>
     </div>
   );
 };
