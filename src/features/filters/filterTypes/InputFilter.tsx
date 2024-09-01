c56ec3715d6541b3f44cf92c81bb4558cf44e849
Sandro <vipsandro11@gmail.com>
2024-09-01 17:37:15 +0400

    update InputFilter.tsx

--- src/features/filters/filterTypes/InputFilter.tsx
+++ src/features/filters/filterTypes/InputFilter.tsx
@@ -7,10 +7,10 @@ const InputFilter = () => {
       <input
         type="text"
         id="search"
-        className="border outline-none px-[2px] text-[10px] h-[20px]"
+        className="border outline-none px-[2px] text-[.9rem] h-fit"
       />
        <button className="absolute right-1 top-1/2 transform -translate-y-1/2">
-        <BiSearch className="size-3" />
+        <BiSearch className="size-5" />
       </button>
     </div>
   );
