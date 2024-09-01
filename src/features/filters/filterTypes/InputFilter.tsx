b55d9e13fba1712da5b20990ab823ce1f3dac60d
Sandro <vipsandro11@gmail.com>
2024-09-01 17:24:39 +0400

    feat: create InputFilter.tsx

---
+++ src/features/filters/filterTypes/InputFilter.tsx
@@ -0,0 +1,14 @@
+const InputFilter = () => {
+  return (
+    <div>
+      <label htmlFor="search"></label>
+      <input
+        type="text"
+        id="search"
+        className="border outline-none px-[1px] h-[20px]"
+      />
+    </div>
+  );
+};
+
+export default InputFilter;
