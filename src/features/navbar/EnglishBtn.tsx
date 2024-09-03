0b0496c2edf7ade322173616acd1f07d6757ee4e
Sandro <vipsandro11@gmail.com>
2024-09-03 15:03:55 +0400

    update EnglishBtn.tsx

--- src/features/navbar/EnglishBtn.tsx
+++ src/features/navbar/EnglishBtn.tsx
@@ -1,12 +1,12 @@
-
-
 const EnglishBtn = () => {
   return (
-    <button className=" text-[.4rem] uppercase text-white bg-secondary p-1
-         mr-4 rounded-sm hover:bg-white hover:text-secondary transition-colors">
-          English Version
-        </button>
-  )
-}
+    <button
+      className=" text-[.8rem] uppercase text-white bg-secondary p-1
+         mr-4 rounded-sm hover:bg-white hover:text-secondary transition-colors"
+    >
+      English Version
+    </button>
+  );
+};
 
-export default EnglishBtn
+export default EnglishBtn;
