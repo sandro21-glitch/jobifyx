f1d6045a43e20ce57800ac18e829fbf08417f699
Sandro <vipsandro11@gmail.com>
2024-09-03 15:50:53 +0400

    update NavList.tsx

--- src/features/navbar/NavList.tsx
+++ src/features/navbar/NavList.tsx
@@ -1,16 +1,16 @@
 const NavList = () => {
   return (
-    <ul className="flex items-center gap-5 text-[.8rem] p-4">
-      <li className="text-white hover:text-accent font-medium cursor-pointer transition-colors">
+    <ul className="flex items-center gap-5 text-[14px] p-4">
+      <li className="text-white hover:text-accent font-semibold cursor-pointer transition-colors">
         განცხადებები
       </li>
-      <li className="text-white hover:text-accent font-medium cursor-pointer transition-colors">
+      <li className="text-white hover:text-accent font-semibold cursor-pointer transition-colors">
         გამოაქვეყნე
       </li>
-      <li className="text-white hover:text-accent font-medium cursor-pointer transition-colors">
+      <li className="text-white hover:text-accent font-semibold cursor-pointer transition-colors">
         კითხვა-პასუხი
       </li>
-      <li className="text-white hover:text-accent font-medium cursor-pointer transition-colors">
+      <li className="text-white hover:text-accent font-semibold cursor-pointer transition-colors">
         კონტაქტი
       </li>
     </ul>
