c43f8a0263a9cda5a0243e4c606fb994acf57e96
Sandro <vipsandro11@gmail.com>
2024-09-04 19:21:08 +0400

    update VipJobCompany.tsx

--- src/features/mainLayout/vipJobs/singleVipJobActions/VipJobCompany.tsx
+++ src/features/mainLayout/vipJobs/singleVipJobActions/VipJobCompany.tsx
@@ -6,6 +6,7 @@ const VipJobCompany = () => {
         src="https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif"
         alt="logo"
         className="absolute -left-16 top-0"
+        loading="lazy"
       />
     </li>
   );
