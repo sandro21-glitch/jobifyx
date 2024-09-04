12dd5ac44764972febb52d95bc193b046a0ce1bb
Sandro <vipsandro11@gmail.com>
2024-09-04 19:20:05 +0400

    update SingleCompanyJob.tsx

--- src/features/mainLayout/companyJobs/SingleCompanyJob.tsx
+++ src/features/mainLayout/companyJobs/SingleCompanyJob.tsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
 
-const SingleCompanyJob = () => {
+type SingleCompanyJobTypes = {
+  company: {
+    companyLogo: string;
+  };
+};
+
+const SingleCompanyJob = ({
+  company: { companyLogo },
+}: SingleCompanyJobTypes) => {
   return (
-    <div>SingleCompanyJob</div>
-  )
-}
+    <li className="w-40 h-40 border flex gap-2 flex-col p-5 justify-center group cursor-pointer">
+      <img
+        src={companyLogo}
+        alt="company logo"
+        className="w-[120px] h-auto"
+        loading="lazy"
+      />
+      <p className="text-[10px] text-center text-primary group-hover:text-gray-500">
+        <Link to="/">ყველა ვაკანსია ერთ გვერდზე</Link>
+      </p>
+    </li>
+  );
+};
 
-export default SingleCompanyJob
+export default SingleCompanyJob;
