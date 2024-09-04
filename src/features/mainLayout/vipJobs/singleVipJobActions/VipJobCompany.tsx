88d9526e626149ad9659c4b8b278cf452b07592f
Sandro <vipsandro11@gmail.com>
2024-09-04 18:25:09 +0400

    feat: create VipJobCompany.tsx

---
+++ src/features/mainLayout/vipJobs/singleVipJobActions/VipJobCompany.tsx
@@ -0,0 +1,14 @@
+const VipJobCompany = () => {
+  return (
+    <li className="col-span-2 relative">
+      არდი დაზღვევა
+      <img
+        src="https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif"
+        alt="logo"
+        className="absolute -left-16 top-0"
+      />
+    </li>
+  );
+};
+
+export default VipJobCompany;
