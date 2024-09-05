0cdbdda8dd983cfa699fdaa3032eae2ad1c3befd
Sandro <vipsandro11@gmail.com>
2024-09-05 22:02:21 +0400

    feat: create SingleJob.tsx

---
+++ src/ui/jobListing/tableComponents/SingleJob.tsx
@@ -0,0 +1,42 @@
+import JobCompany from "./singleJobInfo/JobCompany";
+import JobDeadline from "./singleJobInfo/JobDeadline";
+import JobLink from "./singleJobInfo/JobLink";
+import JobPublishDate from "./singleJobInfo/JobPublishDate";
+
+type SingleJobTypes = {
+  job: {
+    jobId: string;
+    jobTitle: string;
+    jobCompanyName: string;
+    jobCompanyImage: string;
+    jobPublishDate: string;
+    jobDeadline: string;
+  };
+};
+
+const SingleJob = ({
+  job: {
+    jobCompanyImage,
+    jobCompanyName,
+    jobDeadline,
+    jobId,
+    jobPublishDate,
+    jobTitle,
+  },
+}: SingleJobTypes) => {
+  console.log(jobId);
+
+  return (
+    <ul
+      className="grid grid-cols-7 text-[12px] font-semibold text-primary
+  py-2 px-5 hover:bg-gray-100 transition-all ease-in duration-150 border-b"
+    >
+      <JobLink jobTitle={jobTitle} />
+      <JobCompany companyName={jobCompanyName} companyImage={jobCompanyImage} />
+      <JobPublishDate publishDate={jobPublishDate} />
+      <JobDeadline jobDeadline={jobDeadline} />
+    </ul>
+  );
+};
+
+export default SingleJob;
