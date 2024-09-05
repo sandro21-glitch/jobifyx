86ad1ee9f9d6ed483268af81238b23c233dbde0f
Sandro <vipsandro11@gmail.com>
2024-09-05 22:02:30 +0400

    feat: create JobLink.tsx

---
+++ src/ui/jobListing/tableComponents/singleJobInfo/JobLink.tsx
@@ -0,0 +1,30 @@
+import { BiStar } from "react-icons/bi";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { MdOutlineFiberNew } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+type JobLinkTypes = {
+  jobTitle: string;
+};
+const JobLink = ({ jobTitle }: JobLinkTypes) => {
+  return (
+    <li className="col-span-3 relative flex items-center">
+      <button className="absolute -left-5 text-gray-500 text-[16px] mr-5">
+        <BiStar />
+      </button>
+      <Link to={"/"} className="">
+        {jobTitle}
+      </Link>
+      <div className="flex items-center ml-2">
+        <span className="text-[8px] text-gray-500">
+          <FaExternalLinkAlt />
+        </span>
+        <span className="text-[20px] text-red-600">
+          <MdOutlineFiberNew />
+        </span>
+      </div>
+    </li>
+  );
+};
+
+export default JobLink;
