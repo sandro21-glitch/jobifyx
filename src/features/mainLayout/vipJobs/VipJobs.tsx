d9502e6ab8c7e6bacddc08b901f901b9ea0ee642
Sandro <vipsandro11@gmail.com>
2024-09-05 22:01:54 +0400

    update VipJobs.tsx

--- src/features/mainLayout/vipJobs/VipJobs.tsx
+++ src/features/mainLayout/vipJobs/VipJobs.tsx
@@ -1,18 +1,46 @@
-import CompanyJobsList from "../companyJobs/CompanyJobsList";
-import VipJobList from "./VipJobList";
-import VipJobsHeader from "./VipJobsHeader";
+import JobListing from "../../../ui/jobListing/JobListing";
+
+const tempVipJobList = [
+  {
+    jobId: "1",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+  },
+  {
+    jobId: "3",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+  },
+  {
+    jobId: "444",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+  },
+  {
+    jobId: "4441",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+  },
+];
 
 const VipJobs = () => {
-  return (
-    <div className="mt-5">
-      {/* header */}
-      <VipJobsHeader />
-      {/* vip jobs */}
-      <VipJobList />
-      {/* company jobs */}
-      <CompanyJobsList />
-    </div>
-  );
+  return <JobListing isVipJobs={true} jobs={tempVipJobList} />;
 };
 
 export default VipJobs;
