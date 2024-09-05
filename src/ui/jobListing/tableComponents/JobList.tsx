050e3bae59bb1955c17c6bfc982618347bc6ad34
Sandro <vipsandro11@gmail.com>
2024-09-05 22:02:18 +0400

    feat: create JobList.tsx

---
+++ src/ui/jobListing/tableComponents/JobList.tsx
@@ -0,0 +1,23 @@
+import SingleJob from "./SingleJob";
+
+type JobListTypes = {
+  jobs: {
+    jobId: string;
+    jobTitle: string;
+    jobCompanyName: string;
+    jobCompanyImage: string;
+    jobPublishDate: string;
+    jobDeadline: string;
+  }[];
+};
+const JobList = ({ jobs }: JobListTypes) => {
+  return (
+    <ul>
+      {jobs.map((job) => {
+        return <SingleJob key={job.jobId} job={job} />;
+      })}
+    </ul>
+  );
+};
+
+export default JobList;
