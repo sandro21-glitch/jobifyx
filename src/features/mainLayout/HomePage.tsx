02f00034dc6f492016d5390189e203779a475937
Sandro <vipsandro11@gmail.com>
2024-09-05 22:01:32 +0400

    update HomePage.tsx

--- src/features/mainLayout/HomePage.tsx
+++ src/features/mainLayout/HomePage.tsx
@@ -1,9 +1,15 @@
+import CompanyJobsList from "./companyJobs/CompanyJobsList";
+import StandardJobListings from "./standardJobsListing/StandardJobListings";
 import VipJobs from "./vipJobs/VipJobs";
 
 const HomePage = () => {
   return (
     <section className="section-center">
       <VipJobs />
+      {/* company jobs */}
+      <CompanyJobsList />
+      {/* standard jobs */}
+      <StandardJobListings />
     </section>
   );
 };
