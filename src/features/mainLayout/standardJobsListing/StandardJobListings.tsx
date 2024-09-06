d85099834368b42cd5cd5b1166e776e3858b4453
Sandro <vipsandro11@gmail.com>
2024-09-06 19:37:20 +0400

    update StandardJobListings.tsx

--- src/features/mainLayout/standardJobsListing/StandardJobListings.tsx
+++ src/features/mainLayout/standardJobsListing/StandardJobListings.tsx
@@ -9,38 +9,82 @@ const tempStandardJobList = [
       "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
     jobPublishDate: "03 სექტემბერი",
     jobDeadline: "03 ოქტომბერი",
+    isVip: false,
   },
   {
-    jobId: "3",
+    jobId: "1",
     jobTitle: "უფროსი შიდა აუდიტორი",
     jobCompanyName: "არდი დაზღვევა",
     jobCompanyImage:
       "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
     jobPublishDate: "03 სექტემბერი",
     jobDeadline: "03 ოქტომბერი",
+    isVip: false,
   },
   {
-    jobId: "444",
+    jobId: "1",
     jobTitle: "უფროსი შიდა აუდიტორი",
     jobCompanyName: "არდი დაზღვევა",
     jobCompanyImage:
       "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
     jobPublishDate: "03 სექტემბერი",
     jobDeadline: "03 ოქტომბერი",
+    isVip: false,
   },
   {
-    jobId: "4441",
+    jobId: "1",
     jobTitle: "უფროსი შიდა აუდიტორი",
     jobCompanyName: "არდი დაზღვევა",
     jobCompanyImage:
       "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
     jobPublishDate: "03 სექტემბერი",
     jobDeadline: "03 ოქტომბერი",
+    isVip: false,
+  },
+  {
+    jobId: "1",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+    isVip: false,
+  },
+  {
+    jobId: "1",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+    isVip: false,
+  },
+  {
+    jobId: "1",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+    isVip: false,
+  },
+  {
+    jobId: "1",
+    jobTitle: "უფროსი შიდა აუდიტორი",
+    jobCompanyName: "არდი დაზღვევა",
+    jobCompanyImage:
+      "https://jobs.ge/data/clients/logo_icon/6612_logo_for_jobsge_1333557.gif",
+    jobPublishDate: "03 სექტემბერი",
+    jobDeadline: "03 ოქტომბერი",
+    isVip: false,
   },
 ];
 
 const StandardJobListings = () => {
-  return <JobListing isVipJobs={false} jobs={tempStandardJobList} />;
+  return <JobListing jobs={tempStandardJobList} />;
 };
 
 export default StandardJobListings;
