35d5feb1593f44c042540425f57f8871a080d75d
Sandro <vipsandro11@gmail.com>
2024-09-06 19:47:35 +0400

    update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
-import { getDatabase } from "firebase/database"; 
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const database = getDatabase
+export const database = getDatabase(app);
