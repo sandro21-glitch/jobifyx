bad2c3d227b3ddebc596d67cd630aa2da9cf4f15
Sandro <vipsandro11@gmail.com>
2024-09-07 21:37:09 +0400

    feat: create Modals.tsx

---
+++ src/features/modals/Modals.tsx
@@ -0,0 +1,20 @@
+import { useAppSelector } from "../../hooks";
+import PostingAdModal from "./modalTypes/PostingAdModal";
+
+const Modals = () => {
+  const { postingAd } = useAppSelector((store) => store.modals);
+  const isAnyModalOpen = postingAd;
+  if (!isAnyModalOpen) return null;
+  return (
+    <div className="fixed inset-0">
+      <div
+        className={`absolute inset-0 ${
+          postingAd ? "bg-white opacity-100" : "bg-black opacity-60"
+        } z-[-1]`}
+      ></div>
+      {postingAd && <PostingAdModal />}
+    </div>
+  );
+};
+
+export default Modals;
