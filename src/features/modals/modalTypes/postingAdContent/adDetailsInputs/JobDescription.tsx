7ca4598d820781ae8f6c682cadc3cc4889342c46
Sandro <vipsandro11@gmail.com>
2024-09-08 18:31:04 +0400

    feat: create JobDescription.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/adDetailsInputs/JobDescription.tsx
@@ -0,0 +1,11 @@
+import Input from "../../../../../ui/Input";
+
+const JobDescription = () => {
+  return (
+    <div>
+      <Input inputFieldType="text" placeholder="ვაკანსიის აღწერა" />
+    </div>
+  );
+};
+
+export default JobDescription;
