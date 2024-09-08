f8aa567b40732db2f8f92539370532ca967b06ed
Sandro <vipsandro11@gmail.com>
2024-09-08 18:30:57 +0400

    feat: create AdComment.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/adDetailsInputs/AdComment.tsx
@@ -0,0 +1,11 @@
+import Input from "../../../../../ui/Input";
+
+const AdComment = () => {
+  return (
+    <div>
+      <Input inputFieldType="text" placeholder="კომენტარი" />
+    </div>
+  );
+};
+
+export default AdComment;
