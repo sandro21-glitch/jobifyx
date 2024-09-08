4d3e4677a5a2b97c050c86ba7916afe31aee4aab
Sandro <vipsandro11@gmail.com>
2024-09-08 18:31:06 +0400

    feat: create CompanyMail.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyMail.tsx
@@ -0,0 +1,11 @@
+import Input from "../../../../../ui/Input";
+
+const CompanyMail = () => {
+  return (
+    <div>
+      <Input inputFieldType="email" placeholder="კომპანიის ელ-ფოსტა" />
+    </div>
+  );
+};
+
+export default CompanyMail;
