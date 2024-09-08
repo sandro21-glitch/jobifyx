2ebbe745b414ec3888983f676dbe92281cc699df
Sandro <vipsandro11@gmail.com>
2024-09-08 18:31:12 +0400

    feat: create CompanyNumber.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyNumber.tsx
@@ -0,0 +1,11 @@
+import Input from "../../../../../ui/Input";
+
+const CompanyNumber = () => {
+  return (
+    <div>
+      <Input inputFieldType="number" placeholder="ტელეფონი" />
+    </div>
+  );
+};
+
+export default CompanyNumber;
