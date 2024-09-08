5ae4cac2839167e402cc24032fb74976217c2fb9
Sandro <vipsandro11@gmail.com>
2024-09-08 18:31:10 +0400

    feat: create CompanyName.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyName.tsx
@@ -0,0 +1,11 @@
+import Input from "../../../../../ui/Input";
+
+const CompanyName = () => {
+  return (
+    <div>
+      <Input inputFieldType="text" placeholder="კომპანიის დასახელება" />
+    </div>
+  );
+};
+
+export default CompanyName;
