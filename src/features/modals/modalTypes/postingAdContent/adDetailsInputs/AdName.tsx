2956e0ddfcb890e27a95a5f8ced21ba1b4d0caa8
Sandro <vipsandro11@gmail.com>
2024-09-08 18:31:02 +0400

    feat: create AdName.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/adDetailsInputs/AdName.tsx
@@ -0,0 +1,11 @@
+import Input from "../../../../../ui/Input";
+
+const AdName = () => {
+  return (
+    <div>
+      <Input inputFieldType="text" placeholder="განცხადების დასახელება" />
+    </div>
+  );
+};
+
+export default AdName;
