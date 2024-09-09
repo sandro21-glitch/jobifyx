999fd36498ed43bb9668fe5ee1db25c8220fe433
Sandro <vipsandro11@gmail.com>
2024-09-09 22:24:35 +0400

    update AddJobBtn.tsx

--- src/features/modals/modalTypes/postingAdContent/AddJobBtn.tsx
+++ src/features/modals/modalTypes/postingAdContent/AddJobBtn.tsx
@@ -1,6 +1,10 @@
 const AddJobBtn = () => {
   return (
-    <button type="submit" className="w-full bg-primary p-4 text-white mt-5">
+    <button
+      type="submit"
+      className="w-full bg-primary border border-transparent p-4 text-white font-semibold rounded-md tracking-wider mt-5
+       hover:bg-white hover:text-primary hover:border-primary"
+    >
       გაგზავნა
     </button>
   );
