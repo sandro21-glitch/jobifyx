60d3a2a2ad77afc8dba50e31b670c4f4c2db370e
Sandro <vipsandro11@gmail.com>
2024-09-09 15:51:45 +0400

    update JobDescription.tsx

--- src/features/modals/modalTypes/postingAdContent/adDetailsInputs/JobDescription.tsx
+++ src/features/modals/modalTypes/postingAdContent/adDetailsInputs/JobDescription.tsx
@@ -1,9 +1,13 @@
-import Input from "../../../../../ui/Input";
-
 const JobDescription = () => {
   return (
     <div>
-      <Input inputFieldType="text" placeholder="ვაკანსიის აღწერა" />
+      <textarea
+        placeholder="ვაკანსიის აღწერა"
+        className="px-2 py-3 w-full outline-none border 
+      text-black
+      border-gray-400 bg-gray-300 rounded-md
+      focus:border-primary focus:bg-white transition-all ease-in duration-150"
+      />
     </div>
   );
 };
