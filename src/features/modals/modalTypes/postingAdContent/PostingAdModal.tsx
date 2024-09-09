80a8d0b6cc3eed76a1eb3b594317bfb6d1846750
Sandro <vipsandro11@gmail.com>
2024-09-09 15:51:50 +0400

    update PostingAdModal.tsx

--- src/features/modals/modalTypes/postingAdContent/PostingAdModal.tsx
+++ src/features/modals/modalTypes/postingAdContent/PostingAdModal.tsx
@@ -1,12 +1,17 @@
 import AdDetailsInputs from "./adDetailsInputs/AdDetailsInputs";
+import AddJobBtn from "./AddJobBtn";
 import CompanyInfo from "./companyInfoInputs/CompanyInfo";
 import logo from "/logo/logo-no-background.svg";
 
 const PostingAdModal = () => {
   return (
-    <div className="flex flex-col items-center min-h-full  max-w-[40rem] mx-auto my-20">
+    <div className="flex flex-col items-center min-h-full max-w-[35rem] mx-auto my-20">
       <img src={logo} alt="logo" className="max-w-[20rem] h-auto mb-5" />
-      <div className="bg-gray-200 w-full p-5 rounded-md">
+      <div className="bg-gray-200 w-full p-5 rounded-md relative">
+        <div
+          className="border-b-8 border-primary absolute
+         bottom-0 left-0 right-0 rounded-br-lg rounded-bl-lg "
+        ></div>
         <h1 className="text-[1.5rem] font-medium text-center">
           განცხადების განთავსება
         </h1>
@@ -14,6 +19,7 @@ const PostingAdModal = () => {
         <form>
           <CompanyInfo />
           <AdDetailsInputs />
+          <AddJobBtn />
         </form>
       </div>
     </div>
