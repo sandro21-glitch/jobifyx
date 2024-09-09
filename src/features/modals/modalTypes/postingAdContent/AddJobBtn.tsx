c7cf367db46a870854e06ef1ccf8560078517b86
Sandro <vipsandro11@gmail.com>
2024-09-09 15:51:52 +0400

    feat: create AddJobBtn.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/AddJobBtn.tsx
@@ -0,0 +1,9 @@
+const AddJobBtn = () => {
+  return (
+    <button type="submit" className="w-full bg-primary p-4 text-white mt-5">
+      გაგზავნა
+    </button>
+  );
+};
+
+export default AddJobBtn;
