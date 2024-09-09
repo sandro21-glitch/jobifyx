8a77d52103fb25fd6bddbe9938b02dc217dcbb9b
Sandro <vipsandro11@gmail.com>
2024-09-09 15:51:43 +0400

    update Input.tsx

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -17,7 +17,10 @@ const Input = ({
       placeholder={placeholder}
       value={inputValue}
       onChange={onChange}
-      className="px-2 py-4 w-full outline-none border-gray-500 rounded-md"
+      className="px-2 py-3 w-full outline-none border 
+      text-black
+      border-gray-400 bg-gray-300 rounded-md
+      focus:border-primary focus:bg-white transition-all ease-in duration-150"
     />
   );
 };
