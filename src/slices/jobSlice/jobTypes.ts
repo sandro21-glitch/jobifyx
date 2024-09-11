ac6750c79a583ca96d218b041925bb98de1af62f
Sandro <vipsandro11@gmail.com>
2024-09-11 19:46:39 +0400

    update jobTypes.ts

---
+++ src/slices/jobSlice/jobTypes.ts
@@ -0,0 +1,10 @@
+export interface JobTypes {
+  jobId: string;
+  jobTitle: string;
+  jobCompanyName: string;
+  jobCompanyImage: string;
+  jobPublishDate: string;
+  jobDeadline: string;
+  jobImage: string;
+  isVip: boolean;
+}
