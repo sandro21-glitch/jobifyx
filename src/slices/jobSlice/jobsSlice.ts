38bdccf99c4d55c0c7b154507e0329f30b2b4a7f
Sandro <vipsandro11@gmail.com>
2024-09-13 21:50:52 +0400

    update jobsSlice.ts

--- src/slices/jobSlice/jobsSlice.ts
+++ src/slices/jobSlice/jobsSlice.ts
@@ -1,18 +1,38 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { JobTypes } from "./jobTypes";
+import { addJobToDatabase } from "./jobThunks";
 
 interface JobsState {
   jobs: JobTypes[] | null;
+  error: string | null;
+  loading: boolean;
 }
 
 const initialState: JobsState = {
   jobs: null,
+  error: null,
+  loading: false,
 };
 
 export const jobSlice = createSlice({
   name: "job",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addJobToDatabase.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addJobToDatabase.fulfilled, (state, action) => {
+        state.loading = false;
+        state.jobs?.push(action.payload.job);
+      })
+      .addCase(addJobToDatabase.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+  },
 });
 
 export const {} = jobSlice.actions;
