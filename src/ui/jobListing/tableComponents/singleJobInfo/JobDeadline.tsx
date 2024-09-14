4d02f8504bf15666c857c1712aee62edfa0d4610
Sandro <vipsandro11@gmail.com>
2024-09-14 23:23:05 +0400

    update JobDeadline.tsx

--- src/ui/jobListing/tableComponents/singleJobInfo/JobDeadline.tsx
+++ src/ui/jobListing/tableComponents/singleJobInfo/JobDeadline.tsx
@@ -4,7 +4,7 @@ type JobDeadlineTypes = {
 };
 const JobDeadline = ({ jobDeadline, isVip }: JobDeadlineTypes) => {
   return (
-    <li className={`${isVip ? "text-primary" : "text-gray-500"} font-medium`}>
+    <li className={`${isVip ? "text-primary" : "text-gray-500"} font-medium hidden lg:block`}>
       {jobDeadline}
     </li>
   );
