5f0ab637784f241cc17cade6958f9c30ec45a6de
Sandro <vipsandro11@gmail.com>
2024-09-14 23:05:57 +0400

    update HeaderLogo.tsx

--- src/features/header/HeaderLogo.tsx
+++ src/features/header/HeaderLogo.tsx
@@ -1,11 +1,18 @@
+import { Link } from "react-router-dom";
 import logo from "/logo/logo-no-background.svg";
 
 const HeaderLogo = () => {
   return (
     <div className=" section-center flex items-center justify-between">
       <div className="flex items-center">
-        <img src={logo} alt="logo" className="w-[8rem] h-auto py-5" />
-        <div className="w-[1px] h-full bg-primary mx-5 py-5">&nbsp;</div>
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[8rem] h-auto py-5 cursor-pointer"
+          />
+        </Link>
+        <div className="w-[2px] h-full bg-primary mx-5 py-5">&nbsp;</div>
         <p className="text-primary text-[.6rem] font-semibold italic">
           გააუმჯობესეთ თქვენი კარიერული გზა
         </p>
