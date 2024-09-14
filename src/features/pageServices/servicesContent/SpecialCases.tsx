bef693c691876cc1d3830e15e0ee9bb2153b4881
Sandro <vipsandro11@gmail.com>
2024-09-14 19:00:23 +0400

    feat: create SpecialCases.tsx

---
+++ src/features/pageServices/servicesContent/SpecialCases.tsx
@@ -0,0 +1,14 @@
+
+
+const SpecialCases = () => {
+  return (
+    <div className="mb-5">
+      <p className="text-main-p text-gray-500">
+        თუ განცხადება ქვეყნდება ქსელურ მარკეტინგის ჩაბმის მიზნით, ეს პირდაპირ
+        უნდა იყოს მითითებული განცხადების დასაწყისში.
+      </p>
+    </div>
+  );
+};
+
+export default SpecialCases;
