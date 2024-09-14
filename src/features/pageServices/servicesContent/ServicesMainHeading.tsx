e9205f382149a02113c2aacef49f6d19a3fcce35
Sandro <vipsandro11@gmail.com>
2024-09-14 23:10:20 +0400

    update ServicesMainHeading.tsx

--- src/features/pageServices/servicesContent/ServicesMainHeading.tsx
+++ src/features/pageServices/servicesContent/ServicesMainHeading.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ServicesMainHeading = () => {
   return (
     <div className="mb-8">
       <h1 className="text-heading-lg mb-5">გამოაქვეყნე</h1>
       <p className="text-main-p text-gray-500">
-        სამუშაოს სწრაფად გამოსაქვეყნებლად მიჰყევით ამ ბმულს: JobifyX Job Posting
+        სამუშაოს სწრაფად გამოსაქვეყნებლად დააჭირეთ ღილაკს:{" "}
+        <Link to={"/services"} className="text-primary font-bold underline">სამუშაოს დამატება</Link>
         <br />
         ვაკანსიები, რომლებშიც არ არის მითითებული განაცხადის ბოლო ვადა,
         ავტომატურად გამოქვეყნდება 30 დღის განმავლობაში.
