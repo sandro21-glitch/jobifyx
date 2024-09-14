e8e82de43b3ecd1d8703ee04f5e6738e8a246a41
Sandro <vipsandro11@gmail.com>
2024-09-14 19:00:21 +0400

    feat: create ServicesMainHeading.tsx

---
+++ src/features/pageServices/servicesContent/ServicesMainHeading.tsx
@@ -0,0 +1,15 @@
+const ServicesMainHeading = () => {
+  return (
+    <div className="mb-8">
+      <h1 className="text-heading-lg mb-5">გამოაქვეყნე</h1>
+      <p className="text-main-p text-gray-500">
+        სამუშაოს სწრაფად გამოსაქვეყნებლად მიჰყევით ამ ბმულს: JobifyX Job Posting
+        <br />
+        ვაკანსიები, რომლებშიც არ არის მითითებული განაცხადის ბოლო ვადა,
+        ავტომატურად გამოქვეყნდება 30 დღის განმავლობაში.
+      </p>
+    </div>
+  );
+};
+
+export default ServicesMainHeading;
