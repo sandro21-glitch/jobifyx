48d2f2079d4f4aa4bedc6a687beed51f82551396
Sandro <vipsandro11@gmail.com>
2024-09-15 00:36:40 +0400

    update ServicesMainHeading.tsx

--- src/features/pageServices/servicesContent/ServicesMainHeading.tsx
+++ src/features/pageServices/servicesContent/ServicesMainHeading.tsx
@@ -5,8 +5,10 @@ const ServicesMainHeading = () => {
     <div className="mb-8">
       <h1 className="text-heading-lg mb-5">გამოაქვეყნე</h1>
       <p className="text-main-p text-gray-500">
-        სამუშაოს სწრაფად გამოსაქვეყნებლად დააჭირეთ ღილაკს:{" "}
-        <Link to={"/services"} className="text-primary font-bold underline">სამუშაოს დამატება</Link>
+        სამუშაოს გამოსაქვეყნებლად დააჭირეთ ღილაკს:{" "}
+        <Link to={"/services"} className="text-primary font-bold underline">
+          სამუშაოს დამატება
+        </Link>
         <br />
         ვაკანსიები, რომლებშიც არ არის მითითებული განაცხადის ბოლო ვადა,
         ავტომატურად გამოქვეყნდება 30 დღის განმავლობაში.
