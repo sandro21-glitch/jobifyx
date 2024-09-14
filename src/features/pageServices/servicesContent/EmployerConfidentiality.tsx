20681b57d6caf6a90bb13731459fed52ab79b7ee
Sandro <vipsandro11@gmail.com>
2024-09-14 19:00:17 +0400

    feat: create EmployerConfidentiality.tsx

---
+++ src/features/pageServices/servicesContent/EmployerConfidentiality.tsx
@@ -0,0 +1,15 @@
+
+
+const EmployerConfidentiality = () => {
+  return (
+    <div className="mb-5">
+      <p className="text-main-p text-gray-500 ">
+        დამქირავებელი, რომელსაც სურს განცხადებაში არ მიუთითოს საკუთარი ვინაობა
+        და საკონტაქტო ინფორმაცია, ვალდებულია ეს ინფორმაცია მოგვაწოდოს ცალკე.
+        კონფიდენციალობა მკაცრად იქნება დაცული.
+      </p>
+    </div>
+  );
+};
+
+export default EmployerConfidentiality;
