e2825df3e13617170674cf14cbc17ab533201fd5
Sandro <vipsandro11@gmail.com>
2024-09-17 22:50:50 +0400

    update CompanyInfo.tsx

--- src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyInfo.tsx
+++ src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyInfo.tsx
@@ -2,12 +2,37 @@ import CompanyName from "./CompanyName";
 import CompanyMail from "./CompanyMail";
 import CompanyNumber from "./CompanyNumber";
 
-const CompanyInfo = () => {
+type CompanyInfoTypes = {
+  jobCompanyName: string;
+  setJobCompanyName: (val: string) => void;
+  jobCompanyMail: string;
+  setJobCompanyMail: (val: string) => void;
+  jobCompanyNumber: number;
+  setCompanyNumber: (val: number) => void;
+};
+
+const CompanyInfo = ({
+  jobCompanyName,
+  setJobCompanyName,
+  jobCompanyMail,
+  setJobCompanyMail,
+  jobCompanyNumber,
+  setCompanyNumber,
+}: CompanyInfoTypes) => {
   return (
     <div className="flex flex-col gap-5">
-      <CompanyName />
-      <CompanyMail />
-      <CompanyNumber />
+      <CompanyName
+        jobCompanyName={jobCompanyName}
+        setJobCompanyName={setJobCompanyName}
+      />
+      <CompanyMail
+        jobCompanyMail={jobCompanyMail}
+        setJobCompanyMail={setJobCompanyMail}
+      />
+      <CompanyNumber
+        jobCompanyNumber={jobCompanyNumber}
+        setCompanyNumber={setCompanyNumber}
+      />
     </div>
   );
 };
