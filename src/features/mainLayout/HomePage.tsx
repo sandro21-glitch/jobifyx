69f23e7619b27c1f77c8e866e243d36154145560
Sandro <vipsandro11@gmail.com>
2024-09-17 22:50:28 +0400

    update HomePage.tsx

--- src/features/mainLayout/HomePage.tsx
+++ src/features/mainLayout/HomePage.tsx
@@ -2,23 +2,9 @@ import Modals from "../modals/Modals";
 import CompanyJobsList from "./companyJobs/CompanyJobsList";
 import StandardJobListings from "./standardJobsListing/StandardJobListings";
 import VipJobs from "./vipJobs/VipJobs";
-import { useAppDispatch } from "../../hooks";
-import { addJobToDatabase } from "../../slices/jobSlice/jobThunks";
-import { JobTypes } from "../../slices/jobSlice/jobTypes";
 import Filters from "../filters/Filters";
 
 const HomePage = () => {
-  const job: Omit<JobTypes, "jobId"> = {
-    jobTitle: "Test Job Title",
-    jobCompanyName: "Test Company",
-    jobCompanyImage: "IMAGE",
-    jobPublishDate: "testiong",
-    jobDeadline: "testiong",
-    isVip: false,
-  };
-
-  const dispatch = useAppDispatch();
-
   return (
     <section className="section-center">
       {/* page filters */}
@@ -31,9 +17,6 @@ const HomePage = () => {
       <StandardJobListings />
       {/* modals */}
       <Modals />
-      <button onClick={() => dispatch(addJobToDatabase({ job }))}>
-        ADDDDDD
-      </button>
     </section>
   );
 };
