f1d2e29e2b30159eb867fe826333bc9a0549ec22
Sandro <vipsandro11@gmail.com>
2024-09-17 22:50:57 +0400

    update CompanyNumber.tsx

--- src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyNumber.tsx
+++ src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyNumber.tsx
@@ -1,9 +1,25 @@
 import Input from "../../../../../ui/Input";
 
-const CompanyNumber = () => {
+type CompanyNumberTypes = {
+  jobCompanyNumber: number;
+  setCompanyNumber: (val: number) => void;
+};
+
+const CompanyNumber = ({
+  jobCompanyNumber,
+  setCompanyNumber,
+}: CompanyNumberTypes) => {
+  const handleSetCompanyNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCompanyNumber(Number(e.target.value));
+  };
   return (
     <div>
-      <Input inputFieldType="number" placeholder="ტელეფონი" />
+      <Input
+        inputFieldType="number"
+        inputValue={jobCompanyNumber}
+        onChange={handleSetCompanyNumber}
+        placeholder="ტელეფონი"
+      />
     </div>
   );
 };
