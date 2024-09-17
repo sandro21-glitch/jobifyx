763403e0f9c6713e8d59414998268cf2a704417e
Sandro <vipsandro11@gmail.com>
2024-09-17 22:50:40 +0400

    update AdDetailsInputs.tsx

--- src/features/modals/modalTypes/postingAdContent/adDetailsInputs/AdDetailsInputs.tsx
+++ src/features/modals/modalTypes/postingAdContent/adDetailsInputs/AdDetailsInputs.tsx
@@ -3,15 +3,33 @@ import AdName from "./AdName";
 import CompanyImage from "./CompanyImage";
 import JobDescription from "./JobDescription";
 
-const AdDetailsInputs = () => {
+type AdDetailsInputTypes = {
+  jobTitle: string;
+  setJobTitle: (val: string) => void;
+  jobComment: string;
+  setJobComment: (val: string) => void;
+  jobDescription: string;
+  setJobDescription: (val: string) => void;
+};
+const AdDetailsInputs = ({
+  jobTitle,
+  setJobTitle,
+  jobComment,
+  setJobComment,
+  jobDescription,
+  setJobDescription,
+}: AdDetailsInputTypes) => {
   return (
     <div className="mt-7 flex flex-col gap-5">
       <h4 className="text-[1.2rem] font-medium text-left">
         განცხადების ტიპის დასახელება
       </h4>
-      <AdName />
-      <AdComment />
-      <JobDescription />
+      <AdName jobTitle={jobTitle} setJobTitle={setJobTitle} />
+      <AdComment jobComment={jobComment} setJobComment={setJobComment} />
+      <JobDescription
+        jobDescription={jobDescription}
+        setJobDescription={setJobDescription}
+      />
       <CompanyImage />
     </div>
   );
