771bd9e2bc9bbc57f83de5514c822ddad2ba8ba9
Sandro <vipsandro11@gmail.com>
2024-09-17 22:50:55 +0400

    update CompanyName.tsx

--- src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyName.tsx
+++ src/features/modals/modalTypes/postingAdContent/companyInfoInputs/CompanyName.tsx
@@ -1,9 +1,25 @@
 import Input from "../../../../../ui/Input";
 
-const CompanyName = () => {
+type CompanyNameTypes = {
+  jobCompanyName: string;
+  setJobCompanyName: (val: string) => void;
+};
+const CompanyName = ({
+  jobCompanyName,
+  setJobCompanyName,
+}: CompanyNameTypes) => {
+  const handleSetCompanyName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setJobCompanyName(e.target.value);
+  };
+
   return (
     <div>
-      <Input inputFieldType="text" placeholder="კომპანიის დასახელება" />
+      <Input
+        inputFieldType="text"
+        inputValue={jobCompanyName}
+        onChange={handleSetCompanyName}
+        placeholder="კომპანიის დასახელება"
+      />
     </div>
   );
 };
