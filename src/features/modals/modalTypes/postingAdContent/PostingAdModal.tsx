dbcd25a5892f5873f137787b8f79c41adab1ed03
Sandro <vipsandro11@gmail.com>
2024-09-18 18:41:43 +0400

    update PostingAdModal.tsx

--- src/features/modals/modalTypes/postingAdContent/PostingAdModal.tsx
+++ src/features/modals/modalTypes/postingAdContent/PostingAdModal.tsx
@@ -3,7 +3,7 @@ import logo from "/logo/logo-no-background.svg";
 
 const PostingAdModal = () => {
   return (
-    <div className="flex flex-col items-center min-h-full max-w-[35rem] mx-auto">
+    <div className="flex flex-col items-center w-[70rem] max-w-[95vw] mx-auto">
       <img src={logo} alt="logo" className="max-w-[15rem] h-auto my-5" />
       <div className="bg-gray-200 w-full p-5 rounded-md relative">
         <div
