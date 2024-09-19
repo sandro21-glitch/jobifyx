ceb68869155bd67810325752b4669a7b28bbacca
Sandro <vipsandro11@gmail.com>
2024-09-19 19:22:05 +0400

    update JobStatus.tsx

--- src/features/modals/modalTypes/postingAdContent/jobSettings/JobStatus.tsx
+++ src/features/modals/modalTypes/postingAdContent/jobSettings/JobStatus.tsx
@@ -1,10 +1,20 @@
-const JobStatus = () => {
+type JobStatusTypes = {
+  isVip: boolean;
+  setIsVip: (val: boolean) => void;
+};
+
+const JobStatus = ({ isVip, setIsVip }: JobStatusTypes) => {
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsVip(e.target.checked);
+  };
+
   return (
     <div className="flex items-center mb-4">
       <input
         id="default-checkbox"
         type="checkbox"
-        value=""
+        onChange={handleCheckboxChange}
+        checked={isVip}
         className="w-4 h-4 text-blue-600 cursor-pointer bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
       />
       <label
