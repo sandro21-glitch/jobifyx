40c4b56442716f079406a800fa0c40157be397b0
Sandro <vipsandro11@gmail.com>
2024-09-20 22:48:17 +0400

    feat: create JobType.tsx

---
+++ src/features/modals/modalTypes/postingAdContent/jobSettings/JobType.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { jobTypes } from "../../../../../constants/JobTypesList";
+
+const JobType = () => {
+  const [jobCategoryType, setJobCategoryType] = useState<string>("");
+
+  const handleChangeCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setJobCategoryType(e.target.value);
+  };
+
+  return (
+    <div>
+      <label
+        htmlFor="job-duration"
+        className="block text-gray-700 text-sm font-bold mb-2"
+      >
+        აირჩიეთ სამუშაოს კატეგორია
+      </label>
+      <select
+        id="job-duration"
+        value={jobCategoryType}
+        onChange={handleChangeCategory}
+        className="block w-full px-4 text-xs py-2 border border-gray-300 rounded-md focus:outline-none focus:ring outline-none focus:ring-primary focus:border-primary"
+      >
+        {jobTypes.map((type) => {
+          return (
+            <option value={type.type} key={type.id}>
+              {type.type}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default JobType;
