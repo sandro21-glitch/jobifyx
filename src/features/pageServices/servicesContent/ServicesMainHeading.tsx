57934d768fbc2ac63eed71c91a5d14aac3c505e7
Sandro <vipsandro11@gmail.com>
2024-09-22 19:29:14 +0400

    update ServicesMainHeading.tsx

--- src/features/pageServices/servicesContent/ServicesMainHeading.tsx
+++ src/features/pageServices/servicesContent/ServicesMainHeading.tsx
@@ -1,18 +1,28 @@
-import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../../hooks";
+import { setPostingAd } from "../../../slices/modalsSlice/modalsSlice";
 
 const ServicesMainHeading = () => {
+  const dispatch = useAppDispatch();
+
+  // const handelOpenAdModal = () => {
+  //   console.log("work");
+  // };
+
   return (
     <div className="mb-8">
       <h1 className="text-heading-lg mb-5">გამოაქვეყნე</h1>
-      <p className="text-main-p text-gray-500">
+      <div className="text-main-p text-gray-500">
         სამუშაოს გამოსაქვეყნებლად დააჭირეთ ღილაკს:{" "}
-        <Link to={"/services"} className="text-primary font-bold underline">
+        <p
+          onClick={() => dispatch(setPostingAd(true))}
+          className="text-primary font-bold underline inline-block cursor-pointer"
+        >
           სამუშაოს დამატება
-        </Link>
+        </p>
         <br />
         ვაკანსიები, რომლებშიც არ არის მითითებული განაცხადის ბოლო ვადა,
         ავტომატურად გამოქვეყნდება 30 დღის განმავლობაში.
-      </p>
+      </div>
     </div>
   );
 };
