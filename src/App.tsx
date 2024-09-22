34f865039fde459426b2b4e000d47d1920e7e56e
Sandro <vipsandro11@gmail.com>
2024-09-22 19:29:08 +0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./pages/MainLayout";
 import HomePage from "./features/mainLayout/HomePage";
 import Services from "./features/pageServices/Services";
+import Modals from "./features/modals/Modals";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,7 @@ const App = () => {
   return (
     <>
       <RouterProvider router={router} />
+      <Modals />
     </>
   );
 };
