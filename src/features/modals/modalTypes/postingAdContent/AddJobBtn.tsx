1aec6b6d7d319c8618b33c214fe202dc80e08ea2
Sandro <vipsandro11@gmail.com>
2024-09-22 11:00:29 +0400

    update AddJobBtn.tsx

--- src/features/modals/modalTypes/postingAdContent/AddJobBtn.tsx
+++ src/features/modals/modalTypes/postingAdContent/AddJobBtn.tsx
@@ -1,11 +1,16 @@
-const AddJobBtn = () => {
+type AddJobBtnTypes = {
+  uploading: boolean;
+};
+
+const AddJobBtn = ({ uploading }: AddJobBtnTypes) => {
   return (
     <button
       type="submit"
-      className="w-full 
+      disabled={uploading}
+      className={`w-full  ${uploading ? "cursor-not-allowed" : "cursor-pointer"}
        bg-primary border border-transparent p-4 
        text-white font-semibold rounded-md tracking-wider mt-5
-       hover:bg-white hover:text-primary hover:border-primary"
+       hover:bg-white hover:text-primary hover:border-primary`}
     >
       გაგზავნა
     </button>
