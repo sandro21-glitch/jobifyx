b6bae40a4f1fb6e123da097438254e556ffd49ab
Sandro <vipsandro11@gmail.com>
2024-09-24 19:10:29 +0400

    update PostingAdModal.tsx

--- src/features/modals/modalTypes/postingAdContent/PostingAdModal.tsx
+++ src/features/modals/modalTypes/postingAdContent/PostingAdModal.tsx
@@ -1,7 +1,14 @@
 import PostingAdForm from "./PostingAdForm";
 import logo from "/logo/logo-no-background.svg";
+import { useAppDispatch } from "../../../../hooks";
+import { setPostingAd } from "../../../../slices/modalsSlice/modalsSlice";
 
 const PostingAdModal = () => {
+  const dispatch = useAppDispatch();
+  const handleCloseModal = () => {
+    dispatch(setPostingAd(false));
+  };
+
   return (
     <div className="flex flex-col items-center w-[70rem] max-w-[95vw] mx-auto">
       <img src={logo} alt="logo" className="max-w-[15rem] h-auto my-5" />
@@ -10,9 +17,19 @@ const PostingAdModal = () => {
           className="border-b-8 border-primary absolute
          bottom-0 left-0 right-0 rounded-br-lg rounded-bl-lg "
         ></div>
-        <h1 className="text-[1.5rem] font-medium text-center">
-          განცხადების განთავსება
-        </h1>
+        <div className="flex items-center justify-between">
+          <span>&nbsp;</span>
+          <h1 className="text-[1.5rem] font-medium text-center">
+            განცხადების განთავსება
+          </h1>
+          <div
+            onClick={handleCloseModal}
+            className="cursor-pointer group relative w-5 h-5"
+          >
+            <div className="absolute w-1 h-5 inset-0 bg-black -rotate-45 transition-all ease-in duration-150 group-hover:-rotate-90"></div>
+            <div className="absolute w-1 h-5 inset-0 bg-black rotate-45 transition-all ease-in duration-150 group-hover:rotate-90"></div>
+          </div>
+        </div>
         <hr />
         <PostingAdForm />
       </div>
