3df73c6333d21e86373f408b851c74634c316eac
Sandro <vipsandro11@gmail.com>
2024-09-25 19:28:45 +0400

    update JobListing.tsx

--- src/ui/jobListing/JobListing.tsx
+++ src/ui/jobListing/JobListing.tsx
@@ -8,8 +8,13 @@ interface JobListingTypes {
     jobCompanyName: string;
     jobCompanyImage: string;
     jobPublishDate: string;
+    jobCompanyMail: string;
+    jobComment: string;
+    jobDescription: string;
+    jobCompanyNumber: number;
     jobDeadline: string;
     isVip: boolean;
+    jobCategory: string;
   }[];
 }
 
