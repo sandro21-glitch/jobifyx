6d9cb6f2dc73127c84212bc39b65de3558e28664
Sandro <vipsandro11@gmail.com>
2024-09-25 19:28:34 +0400

    feat: create geoMonthNames.ts

---
+++ src/constants/geoMonthNames.ts
@@ -0,0 +1,16 @@
+// Georgian month names
+
+export const georgianMonths = [
+  "იანვარი", // January
+  "თებერვალი", // February
+  "მარტი", // March
+  "აპრილი", // April
+  "მაისი", // May
+  "ივნისი", // June
+  "ივლისი", // July
+  "აგვისტო", // August
+  "სექტემბერი", // September
+  "ოქტომბერი", // October
+  "ნოემბერი", // November
+  "დეკემბერი", // December
+];
