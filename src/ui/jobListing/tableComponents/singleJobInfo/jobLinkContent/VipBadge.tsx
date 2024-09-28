c91d4cbd5b558de3aa2be3e76f58e9b275531b5d
Sandro <vipsandro11@gmail.com>
2024-09-28 20:19:12 +0400

    feat: create VipBadge.tsx

---
+++ src/ui/jobListing/tableComponents/singleJobInfo/jobLinkContent/VipBadge.tsx
@@ -0,0 +1,11 @@
+import { RiVipFill } from "react-icons/ri";
+
+const VipBadge = () => {
+  return (
+    <span className="text-[17px] text-red-600">
+      <RiVipFill />
+    </span>
+  );
+};
+
+export default VipBadge;
