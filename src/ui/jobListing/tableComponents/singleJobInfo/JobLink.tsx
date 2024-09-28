d427a5ab4e4a0fcb1ae8816dcdd7a9983ae73237
Sandro <vipsandro11@gmail.com>
2024-09-28 17:09:32 +0400

    update JobLink.tsx

--- src/ui/jobListing/tableComponents/singleJobInfo/JobLink.tsx
+++ src/ui/jobListing/tableComponents/singleJobInfo/JobLink.tsx
@@ -1,12 +1,29 @@
 import { BiStar } from "react-icons/bi";
 import { FaExternalLinkAlt } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import { RiVipFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+
 type JobLinkTypes = {
   jobTitle: string;
+  jobPublishDate: string;
   isVip: boolean;
 };
-const JobLink = ({ jobTitle, isVip }: JobLinkTypes) => {
+
+const JobLink = ({ jobTitle, isVip, jobPublishDate }: JobLinkTypes) => {
+  // Parse the jobPublishDate using Date constructor
+  const publishDate = new Date(jobPublishDate);
+
+  // Check if the date is valid
+  if (isNaN(publishDate.getTime())) {
+    console.error("Invalid Date:", jobPublishDate);
+    return null;
+  }
+
+  // check if the job is new (published within the last 5 days)
+  const isNewJob =
+    (Date.now() - publishDate.getTime()) / (1000 * 60 * 60 * 24) <= 5;
+
   return (
     <li className="lg:col-span-3 col-span-1 relative flex items-center">
       <button className="absolute -left-5 text-gray-500 text-[16px] mr-5">
@@ -25,6 +42,12 @@ const JobLink = ({ jobTitle, isVip }: JobLinkTypes) => {
         {isVip && (
           <span className="text-[17px] text-red-600">
             <RiVipFill />
+          </span>
+        )}
+        {/* Display "New" badge if the job is new */}
+        {isNewJob && (
+          <span className="text-[9px] text-green-500 ml-1 font-semibold">
+            NEW
           </span>
         )}
       </div>
