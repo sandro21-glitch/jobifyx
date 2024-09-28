d06d865be785c3d1100174bb162e925aaca7a01b
Sandro <vipsandro11@gmail.com>
2024-09-28 21:08:10 +0400

    refactor: :sparkles: utilize React Query for fetching VIP jobs
    - Removed useEffect and Redux state management. - Integrated useQuery for more efficient data fetching and handling. - Simplified error handling with a more descriptive error message. - Set stale time to 5 minutes for better data freshness. - Enhanced code readability by using async/await in the query function.

--- src/features/mainLayout/vipJobs/VipJobs.tsx
+++ src/features/mainLayout/vipJobs/VipJobs.tsx
@@ -1,23 +1,32 @@
-import { useEffect } from "react";
 import JobListing from "../../../ui/jobListing/JobListing";
-import { useAppDispatch, useAppSelector } from "../../../hooks";
 import { fetchVipJobs } from "../../../slices/jobSlice/jobThunks";
+import { useQuery } from "@tanstack/react-query";
+import { JobTypes } from "../../../slices/jobSlice/jobTypes";
+import { useAppDispatch } from "../../../hooks";
 
 const VipJobs = () => {
   const dispatch = useAppDispatch();
 
-  const { data, error, loading } = useAppSelector(
-    (store) => store.jobs.vipJobs
-  );
+  const {
+    data = [],
+    error,
+    isLoading,
+  } = useQuery<JobTypes[], Error>({
+    queryKey: ["vipJobs"],
+    queryFn: async () => {
+      const result = await dispatch(fetchVipJobs());
+      if (fetchVipJobs.fulfilled.match(result)) {
+        return result.payload;
+      }
+      throw new Error(result.error?.message || "Failed to fetch jobs");
+    },
+    staleTime: 1000 * 60 * 5,
+  });
 
-  useEffect(() => {
-    dispatch(fetchVipJobs());
-  }, [dispatch]);
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>error</p>;
-
-  return <JobListing jobs={data ? data : []} />;
+  return <JobListing jobs={data || []} />;
 };
 
 export default VipJobs;
