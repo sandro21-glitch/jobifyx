cbffacd203a06896291f843279be5d2a9a33440a
Sandro <vipsandro11@gmail.com>
2024-09-28 21:07:34 +0400

    refactor: :sparkles: use React Query for data fetching
    - Replaced useEffect and Redux state with useQuery for fetching non-VIP jobs. - Simplified error handling and loading states. - Updated stale time to 5 minutes for improved data freshness. - Enhanced code readability by using async/await in query function.

--- src/features/mainLayout/standardJobsListing/StandardJobListings.tsx
+++ src/features/mainLayout/standardJobsListing/StandardJobListings.tsx
@@ -1,23 +1,28 @@
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../../hooks";
+import { useAppDispatch } from "../../../hooks";
 import JobListing from "../../../ui/jobListing/JobListing";
 import { fetchNonVipJobs } from "../../../slices/jobSlice/jobThunks";
+import { useQuery } from "@tanstack/react-query";
+import { JobTypes } from "../../../slices/jobSlice/jobTypes";
 
 const StandardJobListings = () => {
-  const { data, error, loading } = useAppSelector(
-    (store) => store.jobs.standardJobs
-  );
-
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(fetchNonVipJobs());
-  }, [dispatch]);
+  const { data, isLoading, error } = useQuery<JobTypes[], Error>({
+    queryKey: ["standardJobs"],
+    queryFn: async () => {
+      const res = await dispatch(fetchNonVipJobs());
+      if (fetchNonVipJobs.fulfilled.match(res)) {
+        return res.payload;
+      }
+      throw new Error(res.error?.message || "Failed to fetch jobs");
+    },
+    staleTime: 1000 * 60 * 5,
+  });
 
-  if (loading) return <p>loading...</p>;
+  if (isLoading) return <p>loading...</p>;
   if (error) return <p>error</p>;
 
-  return <JobListing jobs={data ? data : []} />;
+  return <JobListing jobs={data || []} />;
 };
 
 export default StandardJobListings;
