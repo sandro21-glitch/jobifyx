d33fc8fabee771bee16624bbd92b31edc227c5a0
Sandro <vipsandro11@gmail.com>
2024-09-28 23:13:27 +0400

    update jobThunks.ts

--- src/slices/jobSlice/jobThunks.ts
+++ src/slices/jobSlice/jobThunks.ts
@@ -4,36 +4,32 @@ import { equalTo, get, orderByChild, query, ref, set } from "firebase/database";
 import { JobTypes } from "./jobTypes";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { database } from "../../config/firebase";
-import { nanoid } from "nanoid";
 
-// Thunk to add a job with an empty image string
-export const addJobToDatabase = createAsyncThunk<
-  { job: JobTypes },
-  { job: Omit<JobTypes, "jobId"> }
->("jobs/addJobToDatabase", async (payload, { rejectWithValue }) => {
-  const { job } = payload;
+// Thunk to add a job to the database
+export const addJobToDatabase = createAsyncThunk<{ job: JobTypes }, JobTypes>(
+  "jobs/addJobToDatabase",
+  async (job, { rejectWithValue }) => {
+    try {
+      if (!job.jobId) {
+        throw new Error("Failed to generate a new job ID");
+      }
 
-  try {
-    // Generate a new key and set the job data
-    const newJobId = nanoid();
-    if (!newJobId) {
-      throw new Error("Failed to generate a new job ID");
+      const newJob: JobTypes = {
+        ...job,
+      };
+
+      await set(ref(database, `jobs/${job.jobId}`), newJob);
+      console.log("Job added successfully:", newJob);
+
+      return { job: newJob };
+    } catch (error: any) {
+      console.error("Error adding job:", error.message);
+      return rejectWithValue(
+        error.message || "Error adding job to the database"
+      );
     }
-
-    const newJob: JobTypes = {
-      ...job,
-      jobId: newJobId,
-    };
-
-    await set(ref(database, `jobs/${newJobId}`), newJob);
-    console.log("Job added successfully:", newJob);
-
-    return { job: newJob };
-  } catch (error: any) {
-    console.error("Error adding job:", error.message);
-    return rejectWithValue(error.message || "Error adding job to the database");
   }
-});
+);
 
 // Fetch VIP jobs
 export const fetchVipJobs = createAsyncThunk<
