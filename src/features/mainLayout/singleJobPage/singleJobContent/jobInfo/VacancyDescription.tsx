3f8ebdc49e0ecef16dbb92ea98e275f4afbdf756
Sandro <vipsandro11@gmail.com>
2024-09-29 18:11:46 +0400

    feat: create VacancyDescription.tsx

---
+++ src/features/mainLayout/singleJobPage/singleJobContent/jobInfo/VacancyDescription.tsx
@@ -0,0 +1,15 @@
+import ReactMarkdown from "react-markdown";
+
+type VacancyDescriptionTypes = {
+  jobDescription: string;
+};
+
+const VacancyDescription = ({ jobDescription }: VacancyDescriptionTypes) => {
+  return (
+    <div>
+      <ReactMarkdown className="text-[14px] font-medium text-gray-500">{jobDescription}</ReactMarkdown>
+    </div>
+  );
+};
+
+export default VacancyDescription;
