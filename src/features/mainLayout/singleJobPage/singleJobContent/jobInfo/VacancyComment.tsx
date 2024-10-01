6924a71ec9758e84509230f1a243fec3f5c6f2a8
Sandro <vipsandro11@gmail.com>
2024-10-01 17:11:35 +0400

    update VacancyComment.tsx

--- src/features/mainLayout/singleJobPage/singleJobContent/jobInfo/VacancyComment.tsx
+++ src/features/mainLayout/singleJobPage/singleJobContent/jobInfo/VacancyComment.tsx
@@ -4,9 +4,21 @@ type VacancyCommentProps = {
 };
 
 const VacancyComment = ({ jobComment }: VacancyCommentProps) => {
+  const emailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/g;
+
+  const parts = jobComment.split(emailRegex);
+
   return (
-    <div className="text-[14px] font-medium text-gray-500">
-      {jobComment}
+    <div className="text-[14px] font-medium text-gray-500 mt-10">
+      {parts.map((part, index) =>
+        emailRegex.test(part) ? (
+          <span key={index} className="font-bold text-blue-500">
+            {part}
+          </span>
+        ) : (
+          <span key={index}>{part}</span>
+        )
+      )}
     </div>
   );
 };
