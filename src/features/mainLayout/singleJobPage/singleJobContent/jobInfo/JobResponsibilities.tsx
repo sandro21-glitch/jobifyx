6f47ab0101b14316354733747413b8b5eee2abc3
Sandro <vipsandro11@gmail.com>
2024-10-07 20:57:32 +0400

    update JobResponsibilities.tsx

--- src/features/mainLayout/singleJobPage/singleJobContent/jobInfo/JobResponsibilities.tsx
+++ src/features/mainLayout/singleJobPage/singleJobContent/jobInfo/JobResponsibilities.tsx
@@ -1,6 +1,7 @@
 type JobResponsibilitiesTypes = {
   responsibilities: string[];
 };
+
 const JobResponsibilities = ({
   responsibilities,
 }: JobResponsibilitiesTypes) => {
@@ -9,12 +10,12 @@ const JobResponsibilities = ({
       <p className="text-[14px] font-semibold text-gray-500 mb-2">
         ფუნქცია-მოვალეობები:
       </p>
-      <ul>
+      <ul className="list-decimal list-inside flex flex-col gap-1">
         {responsibilities.map((qualification, index) => {
           return (
             <li
               key={index}
-              className="text-[14px] font-medium text-gray-500 list-disc"
+              className="text-[14px] font-medium text-gray-500"
             >
               {qualification}
             </li>
