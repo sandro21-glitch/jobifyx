f5975d66d1ab60d47e10681e1f4a3b105d6996b5
Sandro <vipsandro11@gmail.com>
2024-10-11 22:53:34 +0400

    update Navbar.tsx

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,13 +1,16 @@
 import EnglishBtn from "./EnglishBtn";
 import NavList from "./NavList";
 import FavoriteIcon from "./FavoriteIcon";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="section-center bg-primary rounded-md">
       <nav className="flex items-center justify-between">
         <div className="flex items-center">
-          <FavoriteIcon />
+          <Link to="/favorites">
+            <FavoriteIcon />
+          </Link>
           <NavList />
         </div>
         <EnglishBtn />
