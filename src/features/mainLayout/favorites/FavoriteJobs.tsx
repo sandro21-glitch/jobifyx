3bdac6291c2014853a5b6eda8d22abac4b7a80dc
Sandro <vipsandro11@gmail.com>
2024-10-13 17:34:09 +0400

    update FavoriteJobs.tsx

--- src/features/mainLayout/favorites/FavoriteJobs.tsx
+++ src/features/mainLayout/favorites/FavoriteJobs.tsx
@@ -1,7 +1,14 @@
+import { useAppSelector } from "../../../hooks";
 import JobListing from "../../../ui/jobListing/JobListing";
 
 const FavoriteJobs = () => {
-  return <div>tst</div>
+  const favJobsList = useAppSelector((store) => store.jobs.favoriteJobs);
+
+  return (
+    <section className="section-center">
+      <JobListing jobs={favJobsList} isFav />
+    </section>
+  );
 };
 
 export default FavoriteJobs;
