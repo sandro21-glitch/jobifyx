d9123111129bf64311edcb8686eda51415e3d23f
Sandro <vipsandro11@gmail.com>
2024-10-13 17:34:15 +0400

    update JobHeader.tsx

--- src/ui/jobListing/tableComponents/JobHeader.tsx
+++ src/ui/jobListing/tableComponents/JobHeader.tsx
@@ -1,14 +1,19 @@
 type JobHeaderTypes = {
   isVip: boolean;
+  isFav: boolean;
 };
-const JobHeader = ({ isVip }: JobHeaderTypes) => {
+const JobHeader = ({ isVip, isFav }: JobHeaderTypes) => {
   return (
     <header
       className="grid grid-cols-2 lg:grid-cols-7 text-[14px] font-semibold
      p-2 hover:bg-gray-100 transition-all ease-in duration-150 text-center lg:text-start"
     >
       <p className="lg:col-span-3 text-start md:text-center">
-        {isVip ? "VIP განცხადებები" : "სტანდარტული განცხადებები"}
+        {isFav
+          ? "რჩეულები"
+          : isVip
+          ? "VIP განცხადებები"
+          : "სტანდარტული განცხადებები"}
       </p>
       <p className="lg:col-span-2 text-end md:text-center">მომწოდებელი</p>
       <p className="hidden lg:block">გამოქვეყნდა</p>
